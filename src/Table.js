93c42838bfd8e8574c8e4814112d32829c6b472e
Afaq <afaq.haider1@gmail.com>
2019-02-20 12:28:16 +0500

    seprating components

---
+++ src/Table.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+//
+// class Table extends Component{
+//     render(){
+//         const { user, pattern, onDismis } = this.props;
+//         return (
+//             <div>
+//                 {user.filter(searchUser(pattern)).map(item=> <div key={item.userId}>
+//                     <span>{item.name}</span>
+//                     <span>{item.age}</span>
+//                     <a href={item.address}>{item.name}</a>
+//                     <span>{item.eyecolor}</span>
+//                     <span>{item.height}</span>
+//                     <span>
+//               <button onClick={()=>onDismis(item.userId)} type="button"> DISMISS</button>
+//             </span> </div>)}
+//             </div>
+//         )
+//     }
+// }
+// export default Table;

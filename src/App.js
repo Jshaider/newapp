a8df668e525f15eb0453e05d9c6a26989a278a0e
Afaq <afaq.haider1@gmail.com>
2019-02-21 12:04:43 +0500

    fetch data from server

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Search from './Search';
 import './App.css';
 
 const text= "Welcome To The Road To Learn React";
-const DEFAULT_QUERY = 'redux';
+const DEFAULT_QUERY = 'stephen';
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 const URL=`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+
 
 class App extends Component {
   constructor(props) {
@@ -24,20 +25,22 @@ class App extends Component {
     }
 
     onDismis(id){
-    const isNotid = (item)=> item.userId !==id;
-    const updatedlist= this.state.user.filter(isNotid)
-    this.setState({user:updatedlist});
+        const isNotid = (item)=> item.objectID !==id;
+        const updatedHits= this.state.user.hits.filter(isNotid);
+        this.setState({user: { ...this.state.user, hits: updatedHits }});
     }
     onSearchChange(e){
       this.setState({searchTerm:e.target.value});
+      this.componentDidMount();
     }
     setSearchTopStories(result){
-      this.setState({user:result})
+      this.setState({user:result});
+        console.log(this.state.user);
     }
 
     componentDidMount() {
         const { searchTerm } = this.state;
-        fetch(URL)
+        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
             .then(response => response.json())
             .then(result => this.setSearchTopStories(result))
             .catch(error => error);
@@ -46,7 +49,7 @@ class App extends Component {
 
   render() {
     const {searchTerm,user}=this.state;
-      if (!user) { return null; };
+      if (!user) { return <h1 className="loading">.....Is Loading....</h1>; };
     return (
         <div className="page">
           <h1>{text}</h1>
--- src/Table.js
+++ src/Table.js
@@ -10,7 +10,8 @@ const Table=({ user, pattern, onDismis }) =>
                     <span style={{ width: '20%' }}>{item.title}</span>
                     <span style={{ width: '10%' }}>{item.author}</span>
                     <span style={{width: '30%'}}><a href={item.url}>{item.title}</a></span>
-              <span><button onClick={()=>onDismis(item.userId)} type="button" className="button-inline"> DISMISS</button>
+                    <span style ={{width: '30%'}}> {item.objectID}</span>
+              <span><button onClick={()=>onDismis(item.objectID)} type="button" className="button-inline"> DISMISS</button>
             </span> </div>)}
     </div>)
 export default Table;
